9999ef7c2159ec13b6488a69193f8495dee308f8
unknown <Roger@Roger-PC.(none)>
2011-11-29 20:06:31 +0800

    add lineTool

--- XPaint.Win8/js/app.js
+++ XPaint.Win8/js/app.js
@@ -1,15 +1,40 @@
-﻿var App = function (toolbox, canvas) {
+﻿function App(toolbox, canvas) {
 
-    this.init = function () {
+    this._tools = new Object();
 
-        canvas.html();
+    this._toolbox = toolbox;
+    this._canvas = canvas;
 
-        //var picker = new Storage.Pickers.FileOpenPicker();
-        //picker.fileTypeFilter.append(".jpg");
-        //picker.pickSingleFileAsync()
-        //.then(function (fs) {
-        //    return fs.openAsync(Storage.FileAccessMode.read);
-        //});
-
-    }
+    var context = this._canvas.getContext("2d");
+    this._curTool = new lineTool(5, "#FA0000");
+    this._curTool.draw(context);
 };
+
+App.prototype.loadControls = function () {
+
+    var dialog = new Windows.UI.Popups.MessageDialog(this._toolbox);
+    dialog.showAsync();
+
+}
+
+App.prototype.initEvents = function () {
+    var tool = this._curTool;
+
+    this._canvas.onmousedown = function (e) {
+        tool.onmousedown(e);
+    }
+    this._canvas.onmousemove = function (e) {
+        tool.onmousemove(e);
+    }
+    this._canvas.onmouseup = function (e) {
+        tool.onmouseup(e);
+    }
+}
+
+App.prototype.init = function () {
+    
+    this.loadControls();
+    this.initEvents();
+}
+
+
---
+++ XPaint.Win8/js/lineTool.js
@@ -0,0 +1,40 @@
+﻿
+function lineTool(penSize, color) {
+    this._penSize = penSize;
+    this._color = color;
+
+    this.onmousedown = function (e) {
+        this._isMouseDown = true;
+
+        this._startX = e.pageX;
+        this._startY = e.pageY;
+    }
+    this.onmouseup = function (e) {
+        this._isMouseDown = false;
+
+        var x = e.pageX;
+        var y = e.pageY;
+
+        var context = this._context;
+
+        context.moveTo(this._startX, this._startY);
+        context.lineTo(x, y);
+        context.stroke();
+    }
+}
+
+lineTool.prototype = tool.prototype;
+lineTool.prototype.base = tool;
+lineTool.constructor = lineTool;
+
+lineTool.prototype.draw = function (context) {
+    context.lineWidth = this._penSize;
+    context.strokeStyle = this._color;
+    this._context = context;
+}
+
+lineTool.prototype.onmousemove = function (e) {
+    if (!this._isMouseDown) return;
+
+
+}
