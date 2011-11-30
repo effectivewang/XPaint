d1aa7670b11e129f0e74a3ab613c555b42251aa9
unknown <Roger@Roger-PC.(none)>
2011-11-30 22:47:17 +0800

    add images and tool_map

--- XPaint.Win8/js/app.js
+++ XPaint.Win8/js/app.js
@@ -1,9 +1,44 @@
-﻿function App(toolbox, canvas) {
+﻿var hash_map = {
 
-    this._tools = new Object();
+    get: function (key) {
+        return this[key]
+    },
+
+    set: function (key, value) {
+        this[key] = value
+    },
+
+    contains: function (key) {
+        return this[key] == null ? true : false
+    },
+
+    remove: function (key) {
+        delete this[key]
+    }
+}
+
+function App(toolbox, canvas) {
+
+    this._tools = new Array();
 
     this._toolbox = toolbox;
     this._canvas = canvas;
+
+    this.tool_map = [
+        ["lineTool", "images/tools/Brush_2.png"],
+        ["fillTool", "images/tools/Fill_2.png"],
+        ["cropTool", "images/tools/Crop_2.png"],
+        ["eraserTool", "images/tools/Eraser_2.png"],
+        ["pickerTool", "images/tools/Picker_2.png"],
+        ["ellipseTool", "images/tools/Shape_ellipses_2.png"],
+        ["burstTool", "images/tools/Shape_burst_2.png"],
+        ["gearTool", "images/tools/Shape_gear_2.png"],
+        ["polygonTool", "images/tools/Shape_polygon_2.png"],
+        ["starTool", "images/tools/Shape_star_2.png"],
+        ["stampTool", "images/tools/Stamp_2.png"],
+        ["textTool", "images/tools/Text_2.png"],
+        ["selectTool", "images/tools/Marquee_ellipses_2.png"]
+    ];
 
     var context = this._canvas.getContext("2d");
     this._curTool = new lineTool(5, "#FA0000");
@@ -12,8 +47,16 @@
 
 App.prototype.loadControls = function () {
 
-    var dialog = new Windows.UI.Popups.MessageDialog(this._toolbox);
-    dialog.showAsync();
+    for (i = 0; i < this.tool_map.length; i++) {
+        //var item = this.tool_map[i];
+        var toolName = item[0];
+        var iconFile = item[1];
+
+        //var tool = eval("new " + toolName + "()");
+        //this._tools.push(tool);
+
+        this._toolbox.append("<img id='" + toolName + "' src='" + iconFile + "' />");
+    }
 
 }
 
@@ -32,7 +75,7 @@ App.prototype.initEvents = function () {
 }
 
 App.prototype.init = function () {
-    
+
     this.loadControls();
     this.initEvents();
 }
